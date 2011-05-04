762c466a8733d6bbfe7b767430bd622e2fd19d30
Sean Upton <sdupton@gmail.com>
2011-05-04 13:42:59 -0600

    uu.workflows local roles for data/form entry

---
+++ uu/workflows/sharing.py
@@ -0,0 +1,10 @@
+from zope.interface import implements
+from plone.app.workflow.interfaces import ISharingPageRole
+from Products.CMFPlone import PloneMessageFactory as _
+
+
+class FormEntryRole(object):
+    implements(ISharingPageRole)
+    title = _(u'title_can_enter_data', default=u'Can enter data')
+    required_permission = 'Enter Data'
+
